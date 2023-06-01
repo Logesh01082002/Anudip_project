840ee0b6389b186f832310a9d99b69a0b961b0cb
LOGESH E <113497045+Logesh01082002@users.noreply.github.com>
2023-06-01 23:04:22 +0530

    BookService.java

--- library1project/src/main/java/com/anudip/library1project/services/BookService.java
+++ library1project/src/main/java/com/anudip/library1project/services/BookService.java
@@ -1,17 +1,12 @@
 package com.anudip.library1project.services;
-
-
 import java.util.List;
-
 import com.anudip.library1project.models.Book;
-
 
 public interface BookService
 {
-	public Book saveBook(Book book);
-	public List<Book> getAllBooks();
-	public Book getBookById(Long id);
-	public Book updateBook(Long id, Book book);
-	public void deleteBook(Long id);
-	
+	 Book saveBook(Book book);
+	 List<Book> getAllBooks();
+	 Book getBookById(long id);
+	 Book updateBook(long id, Book book);
+	 void deleteBook(long id);
 }
