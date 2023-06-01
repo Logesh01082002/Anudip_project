800d186dd130204013c75a8174cf7c61ed462955
LOGESH E <113497045+Logesh01082002@users.noreply.github.com>
2023-06-01 22:58:01 +0530

    BookController.java

--- library1project/src/main/java/com/anudip/library1project/controller/BookController.java
+++ library1project/src/main/java/com/anudip/library1project/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.anudip.library1project.controller;
 
-
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -17,43 +16,47 @@ import org.springframework.web.bind.annotation.RestController;
 import com.anudip.library1project.models.Book;
 import com.anudip.library1project.services.BookService;
 
-
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
     private BookService bookService;
 
     public BookController(BookService bookService) {
+    	super();
         this.bookService = bookService;
     }
 
-    @PostMapping("/new")
+    //create book REST API 
+    @PostMapping()
     public ResponseEntity<Book> saveBook(@RequestBody Book book) {
         Book savedBook = bookService.saveBook(book);
         return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
     }
 
-    @GetMapping
+ //build to get book by id REST API    
+    @GetMapping("/Allbooks")
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
+    //build to get book by id REST API
+    //http://localhost:8080/api/books/1
     @GetMapping("/{bid}")
-    public ResponseEntity<Book> getBookById(@PathVariable("bid") Long id) {
-        Book book = bookService.getBookById(id);
+    public ResponseEntity<Book> getBookById(@PathVariable("bid") long bid) {
+        Book book = bookService.getBookById(bid);
         return new ResponseEntity<>(book, HttpStatus.OK);
     }
 
     @PutMapping("/{bid}")
-    public ResponseEntity<Book> updateBook(@PathVariable("id") Long id, @RequestBody Book book) 
+    public ResponseEntity<Book> updateBook(@PathVariable("bid") long bid, @RequestBody Book book) 
     {
-        Book updatedBook = bookService.updateBook(id, book);
+        Book updatedBook = bookService.updateBook(bid, book);
         return new ResponseEntity<>(updatedBook, HttpStatus.OK);
     }
     
     @DeleteMapping("/{bid}")
-    public ResponseEntity<String> deleteBook(@PathVariable("id") Long id) {
-        bookService.deleteBook(id);
+    public ResponseEntity<String> deleteBook(@PathVariable("bid") long bid) {
+        bookService.deleteBook(bid);
         return new ResponseEntity<>("Book deleted successfully!", HttpStatus.OK);
     }
 }
