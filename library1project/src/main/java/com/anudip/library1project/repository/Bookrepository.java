54a99485d2cf212ba84104be912d1a6db1db2bc8
LOGESH E <113497045+Logesh01082002@users.noreply.github.com>
2023-06-30 21:51:30 +0530

    Delete library1project directory

--- library1project/src/main/java/com/anudip/library1project/repository/Bookrepository.java
+++
@@ -1,7 +0,0 @@
-package com.anudip.library1project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.anudip.library1project.models.Book;
-
-public interface Bookrepository extends JpaRepository<Book, Long> {
-}
