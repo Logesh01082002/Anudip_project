736e447fb4d9f93416e0ca5669f3fdc7e251f7e8
LOGESH E <113497045+Logesh01082002@users.noreply.github.com>
2023-07-25 22:44:23 +0530

    delete

--- sprint1libpro/src/main/java/com/AnudipSprint/lms/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AnudipSprint.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.AnudipSprint.lms.entity.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-    // Custom query methods, if needed
-}
